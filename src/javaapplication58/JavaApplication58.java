12d6be79d6bed28a8c4716dbf826aadae8f88588
Walaa Alsulami <wsalsulami0001@stu.kau.edu.sa>
2023-09-06 22:13:49 +0300

    First commit

---
+++ src/javaapplication58/JavaApplication58.java
@@ -0,0 +1,12 @@
+
+package javaapplication58;
+
+
+public class JavaApplication58 {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("walaa's repository");
+    }
+    
+}
